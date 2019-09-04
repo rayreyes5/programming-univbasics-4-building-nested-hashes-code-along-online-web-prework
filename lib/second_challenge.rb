2b7e20295bd461a31c9472ecd7b015de30e8ca30
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 03:31:05 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+    :patriarch => {Lord Montague},
+    :matriarch => {Lady Montague},
+    :hero => {Romeo},
+    :hero_friends => [Bevolio],
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {Lord Capulet},
+      :matriarch => {Lady Capulet},
+      :heroine => {Juliet},
+      :heroine_friends => [Steven, Nurse],
+    }
+      
 
   
 end
--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,27 +2,42 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+         :name => "Lord Montague",
+      :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+          :name => "Lady Montague",
+      :age => "54"
         },
       :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
+       :name => "Romeo",
+      :age => "15"
+      :status => "alive"
+      :hero_friends => [  
+        {
+         :name => "Benvolio",
+         :age => "17",
+         
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet",
+      :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+         :name => "Lady Capulet",
+      :age => "51"
         },
       :heroine => {
-        #your key/value pairs here
+       :name => "Juliet",
+      :age => "15",
+      :status => "alive"
         },
       :heroine_friends => []
+      :name => "Steven",
+        :age => "30",
+      :name => "Nurse",
+        :age => "44",
    }
   }
 
