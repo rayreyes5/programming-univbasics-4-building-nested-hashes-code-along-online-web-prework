bb4e6d020d41968ceecf6922cb992433d5022a5f
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 03:25:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,7 +12,7 @@ def third_challenge
       :hero => {
        :name => "Romeo",
       :age => "15"
-      
+      :status => "alive"
       :hero_friends => [  
         {
          :name => "Benvolio",
@@ -31,6 +31,7 @@ def third_challenge
       :heroine => {
        :name => "Juliet",
       :age => "15",
+      
         },
       :heroine_friends => []
       :name => "Steven",
