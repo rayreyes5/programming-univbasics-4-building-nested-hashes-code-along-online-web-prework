e98c1859da2e515f9386dced5341acb23d318acf
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 03:11:19 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-    :patriarch=> {    Lady Montague :matriarch
-     Romeo :hero 
-     Benvolio :hero_friends
+    :patriarch => {Lord Montague},
+    :matriarch => {Lady Montague},
+    :hero => {Romeo},
+    :hero_friends => {Benvolio}
       }, 
     :capulet => {
       #your key/value pairs here!
